af749282fee3eb15eed0bc0531ea359c22e2d8de
Jiwon <jiwony933@naver.com>
2022-09-22 14:56:24 +0900

    ADD : My Page wip

---
+++ src/Pages/MyPage/MyPage.js
@@ -0,0 +1,113 @@
+import React, { useEffect, useState } from 'react';
+import ModalPortal from '../../Portal';
+import ProfileEdit from './ProfileEdit';
+import './MyPage.scss';
+import LikedItem from './LikedItem';
+import ReviewedItem from './ReviewedItem';
+import { useNavigate } from 'react-router-dom';
+
+const MyPage = () => {
+  const [user, setUser] = useState({});
+  const [editClicked, setEditClicked] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    fetch('./jiwonData/loginUser.json')
+      .then(res => res.json())
+      .then(data => setUser(data));
+  }, []);
+
+  const editClick = () => {
+    setEditClicked(true);
+  };
+
+  const logOutClick = () => {
+    localStorage.removeItem('key', '키값');
+    navigate('/');
+  };
+
+  const gender = () => {
+    if (user.user_gender === 'female') {
+      return '여성';
+    } else if (user.user_gender === 'male') {
+      return '남성';
+    } else if (user.user_gender == null) {
+      return null;
+    }
+  };
+
+  return (
+    <div className="myPage">
+      <div className="myPageBox">
+        <div className="myPageTitle">마이 페이지</div>
+        <div className="myPageMiniTitle">사용자 정보</div>
+        <div className="myPageUserInfo">
+          <img
+            className="userProfileImg"
+            alt="프로필 사진"
+            src={user.user_profile_img}
+          />
+          <div className="userProfileText">
+            <span className="userNickname">
+              {user.user_nickname}님, 안녕하세요!
+            </span>
+            <span className="userId">@ {user.user_id}</span>
+            <span className="userAgeAndGender">
+              {user.user_age}대 {gender(user.user_gender)}
+            </span>
+          </div>
+          <div className="buttonSet">
+            <button className="profileEditBtn" onClick={editClick}>
+              프로필 수정
+            </button>
+            <button className="logOutBtn" onClick={logOutClick}>
+              로그아웃
+            </button>
+          </div>
+        </div>
+        <div className="myActions">
+          <div className="myActionBox">
+            <div className="myPageMiniTitle">내가 좋아요 한 식당</div>
+            <div className="myPageItemList">
+              <LikedItem />
+              <LikedItem />
+              <LikedItem />
+              <LikedItem />
+              <LikedItem />
+              <LikedItem />
+              <LikedItem />
+              <LikedItem />
+              <LikedItem />
+              <LikedItem />
+            </div>
+          </div>
+          <div className="myActionBox">
+            <div className="myPageMiniTitle">나의 예약 리스트</div>
+            <div className="myPageItemList">
+              <LikedItem />
+              <LikedItem />
+              <LikedItem />
+              <LikedItem />
+              <LikedItem />
+            </div>
+          </div>
+          <div className="myActionBox2">
+            <div className="myPageMiniTitle">내가 작성한 후기</div>
+            <div className="myPageItemList">
+              <ReviewedItem />
+              <ReviewedItem />
+              <ReviewedItem />
+              <ReviewedItem />
+              <ReviewedItem />
+            </div>
+          </div>
+        </div>
+      </div>
+      <ModalPortal>
+        {editClicked && <ProfileEdit setEditClicked={setEditClicked} />}
+      </ModalPortal>
+    </div>
+  );
+};
+
+export default MyPage;
