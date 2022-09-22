740975df0d457bca414170d4bfc605c2be08f1f9
SungJongHwa <jonghwasung@gamail.com>
2022-09-22 17:09:10 +0900

    WIP

---
+++ src/Pages/AdminPage/AdminPage.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './AdminPage.scss';
+
+const AdminPage = () => {
+  return (
+    <>
+      <div className="myStoreList">내 가게 리스트</div>
+      <div className="storeList">
+        <div className="store">ㅇㅇㅇ</div>
+        <div className="store">ㅇㅇㅇ</div>
+        <div className="store">ㅇㅇㅇ</div>
+        <div className="store">ㅇㅇㅇ</div>
+        <div className="store">ㅇㅇㅇ</div>
+        <div className="store">ㅇㅇㅇ</div>
+        <div className="store">ㅇㅇㅇ</div>
+        <div className="store">ㅇㅇㅇ</div>
+      </div>
+    </>
+  );
+};
+
+export default AdminPage;
