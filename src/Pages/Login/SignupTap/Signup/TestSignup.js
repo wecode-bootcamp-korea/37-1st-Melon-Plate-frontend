ab4184b2d0ddcb0fe9e54d150883a95fd7810151
SungJongHwa <jonghwasung@gamail.com>
2022-09-25 17:25:16 +0900

    WIP

--- src/Pages/Login/SignupTap/Signup/TestSignup.js
+++ src/Pages/Login/SignupTap/Signup/TestSignup.js
@@ -1,10 +1,7 @@
 import React, { useState, useRef } from 'react';
-import { Navigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 // import { useNavigate } from 'react-router-dom';
 import './Signup.scss';
-
-const navigate = Navigate();
 
 const TestSignup = ({ currentId }) => {
   const [imageSrc, setImageSrc] = useState('');
