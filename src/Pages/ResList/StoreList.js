cfce98ac3d32ef756b48ba3922d5a9dd9caa18bb
Jiwon <jiwony933@naver.com>
2022-09-26 16:40:17 +0900

    wip

--- src/Pages/ResList/StoreList.js
+++ src/Pages/ResList/StoreList.js
@@ -10,27 +10,13 @@ const StoreList = () => {
           <span>믿고 먹는 인기 맛집 ! </span>
           <span>조회수가 높은 식당을 한눈에 확인하세요</span>
         </div>
+        <div className="storeHashTags">
+          {HASHTAG.map(title => (
+            <button className="storeHashTagItem">#{title}</button>
+          ))}
+        </div>
+
         <div className="menuItemList">
-          <MenuItem />
-          <MenuItem />
-          <MenuItem />
-          <MenuItem />
-          <MenuItem />
-          <MenuItem />
-          <MenuItem />
-          <MenuItem />
-          <MenuItem />
-          <MenuItem />
-          <MenuItem />
-          <MenuItem />
-          <MenuItem />
-          <MenuItem />
-          <MenuItem />
-          <MenuItem />
-          <MenuItem />
-          <MenuItem />
-          <MenuItem />
-          <MenuItem />
           <MenuItem />
         </div>
       </div>
@@ -39,3 +25,14 @@ const StoreList = () => {
 };
 
 export default StoreList;
+
+const HASHTAG = [
+  '전체',
+  '선릉',
+  '주점',
+  '파스타',
+  '떡볶이',
+  '해장',
+  '데이트',
+  '회식 맛집',
+];
