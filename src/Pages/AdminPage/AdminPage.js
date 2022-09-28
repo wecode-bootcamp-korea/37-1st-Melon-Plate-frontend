50bfeebf9fde338264979936b605cb3402d65dfe
SungJongHwa <jonghwasung@gamail.com>
2022-09-28 11:09:26 +0900

    WIP

--- src/Pages/AdminPage/AdminPage.js
+++ src/Pages/AdminPage/AdminPage.js
@@ -35,17 +35,17 @@ const AdminPage = () => {
         </button>
       </div>
       <div className="storeList">
-        {/* {text?.map(item => {
+        {text?.map(item => {
           return (
             <Link key={item.id} to={`/user/admin/${item.id}`}>
               <Store text={item} />;
             </Link>
           );
-        })} */}
-
-        {storeData?.map(item => {
-          return <Store text={item} key={item.id} />;
         })}
+
+        {/* {storeData?.map(item => {
+          return <Store text={item} key={item.id} />;
+        })} */}
       </div>
     </>
   );
@@ -53,51 +53,51 @@ const AdminPage = () => {
 
 export default AdminPage;
 
-// const text = [
-//   {
-//     id: 1,
-//     image:
-//       'https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMjA5MTVfMjA4%2FMDAxNjYzMjA0NTE3OTI1.kgWpmxVdE8Ae7GtvGmswN320KHLVjxPGs0rw6N3TZpYg.qwriTz25DKbl8iKC7foCPpkqdakDcpn38yRA_hb-0eog.JPEG.kciis33%2FKakaoTalk_20220915_100312785_15.jpg&type=a340',
-//     name: '차이나',
-//     rate: '4.5',
-//     address: '주소 : 	서울특별시 용산구 한강대로40길 14 1F 104호',
-//     tel: '전화번호 : 	010-2757-3592',
-//     open_time: '영업시간 : 	17:00 - 23:00',
-//     closed_time: 'ddddddddddd',
-//     closed_day: '휴일 : 수',
-//     category_id: '중국음식',
-//   },
-//   {
-//     id: 2,
-//     name: '성종화',
-//     rate: '4.5',
-//     address: '주소 : 	서울특별시 용산구 한강대로40길 14 1F 104호',
-//     tel: '전화번호 : 	010-2757-3592',
-//     open_time: '영업시간 : 	17:00 - 23:00',
-//     closed_time: 'ddddddddddd',
-//     closed_day: '휴일 : 수',
-//     category_id: '중국음식',
-//   },
-//   {
-//     id: 3,
-//     name: '김지원',
-//     rate: '4.5',
-//     address: '주소 : 	서울특별시 용산구 한강대로40길 14 1F 104호',
-//     tel: '전화번호 : 	010-2757-3592',
-//     open_time: '영업시간 : 	17:00 - 23:00',
-//     closed_time: 'ddddddddddd',
-//     closed_day: '휴일 : 수',
-//     category_id: '중국음식',
-//   },
-//   {
-//     id: 4,
-//     name: '차이나',
-//     rate: '4.5',
-//     address: '주소 : 	서울특별시 용산구 한강대로40길 14 1F 104호',
-//     tel: '전화번호 : 	010-2757-3592',
-//     open_time: '영업시간 : 	17:00 - 23:00',
-//     closed_time: 'ddddddddddd',
-//     closed_day: '휴일 : 수',
-//     category_id: '중국음식',
-//   },
-// ];
+const text = [
+  {
+    id: 1,
+    image:
+      'https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMjA5MTVfMjA4%2FMDAxNjYzMjA0NTE3OTI1.kgWpmxVdE8Ae7GtvGmswN320KHLVjxPGs0rw6N3TZpYg.qwriTz25DKbl8iKC7foCPpkqdakDcpn38yRA_hb-0eog.JPEG.kciis33%2FKakaoTalk_20220915_100312785_15.jpg&type=a340',
+    name: '차이나',
+    rate: '4.5',
+    address: '주소 : 	서울특별시 용산구 한강대로40길 14 1F 104호',
+    tel: '전화번호 : 	010-2757-3592',
+    open_time: '영업시간 : 	17:00 - 23:00',
+    closed_time: 'ddddddddddd',
+    closed_day: '휴일 : 수',
+    category_id: '중국음식',
+  },
+  {
+    id: 2,
+    name: '성종화',
+    rate: '4.5',
+    address: '주소 : 	서울특별시 용산구 한강대로40길 14 1F 104호',
+    tel: '전화번호 : 	010-2757-3592',
+    open_time: '영업시간 : 	17:00 - 23:00',
+    closed_time: 'ddddddddddd',
+    closed_day: '휴일 : 수',
+    category_id: '중국음식',
+  },
+  {
+    id: 3,
+    name: '김지원',
+    rate: '4.5',
+    address: '주소 : 	서울특별시 용산구 한강대로40길 14 1F 104호',
+    tel: '전화번호 : 	010-2757-3592',
+    open_time: '영업시간 : 	17:00 - 23:00',
+    closed_time: 'ddddddddddd',
+    closed_day: '휴일 : 수',
+    category_id: '중국음식',
+  },
+  {
+    id: 4,
+    name: '차이나',
+    rate: '4.5',
+    address: '주소 : 	서울특별시 용산구 한강대로40길 14 1F 104호',
+    tel: '전화번호 : 	010-2757-3592',
+    open_time: '영업시간 : 	17:00 - 23:00',
+    closed_time: 'ddddddddddd',
+    closed_day: '휴일 : 수',
+    category_id: '중국음식',
+  },
+];
