da1cdc6a9b03765a200ae71406d5d5d2a4177254
SungJongHwa <jonghwasung@gamail.com>
2022-09-28 15:30:34 +0900

    AdminPage 수정사항 반영완료

--- src/Pages/AdminPage/AdminPage.js
+++ src/Pages/AdminPage/AdminPage.js
@@ -2,8 +2,10 @@ import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import './AdminPage.scss';
 import Store from './Store';
+import { TEXT } from './TEXT';
 
 const AdminPage = () => {
+  //통신시 사용
   const [storeData, setStoreData] = useState([]);
 
   useEffect(() => {
@@ -19,13 +21,12 @@ const AdminPage = () => {
       })
       .then(data => setStoreData(data));
   }, []);
+  //
 
   const navigate = useNavigate();
   const goToAdminAdd = () => {
     navigate('/admin/create');
   };
-  console.log(storeData);
-
   return (
     <>
       <div className="myStoreList">내 가게 리스트</div>
@@ -35,69 +36,18 @@ const AdminPage = () => {
         </button>
       </div>
       <div className="storeList">
-        {/* {text?.map(item => {
-          return (
-            <Link key={item.id} to={`/user/admin/${item.id}`}>
-              <Store text={item} />;
-            </Link>
-          );
-        })} */}
-
-        {storeData?.map(item => {
-          return <Store text={item} key={item.id} />;
+        {TEXT?.map(item => {
+          //통신이 안될시 mock데이터로 확인하는 코드//
+          return <Store TEXT={item} key={item.id} />;
         })}
+
+        {/* 통신시 확인하는 코드 */}
+        {/* {storeData?.map(item => {
+          return <Store text={item} key={item.id} />;
+        })} */}
       </div>
     </>
   );
 };
 
 export default AdminPage;
-
-// const text = [
-//   {
-//     id: 1,
-//     image:
-//       'https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMjA5MTVfMjA4%2FMDAxNjYzMjA0NTE3OTI1.kgWpmxVdE8Ae7GtvGmswN320KHLVjxPGs0rw6N3TZpYg.qwriTz25DKbl8iKC7foCPpkqdakDcpn38yRA_hb-0eog.JPEG.kciis33%2FKakaoTalk_20220915_100312785_15.jpg&type=a340',
-//     name: '차이나',
-//     rate: '4.5',
-//     address: '주소 : 	서울특별시 용산구 한강대로40길 14 1F 104호',
-//     tel: '전화번호 : 	010-2757-3592',
-//     open_time: '영업시간 : 	17:00 - 23:00',
-//     closed_time: 'ddddddddddd',
-//     closed_day: '휴일 : 수',
-//     category_id: '중국음식',
-//   },
-//   {
-//     id: 2,
-//     name: '성종화',
-//     rate: '4.5',
-//     address: '주소 : 	서울특별시 용산구 한강대로40길 14 1F 104호',
-//     tel: '전화번호 : 	010-2757-3592',
-//     open_time: '영업시간 : 	17:00 - 23:00',
-//     closed_time: 'ddddddddddd',
-//     closed_day: '휴일 : 수',
-//     category_id: '중국음식',
-//   },
-//   {
-//     id: 3,
-//     name: '김지원',
-//     rate: '4.5',
-//     address: '주소 : 	서울특별시 용산구 한강대로40길 14 1F 104호',
-//     tel: '전화번호 : 	010-2757-3592',
-//     open_time: '영업시간 : 	17:00 - 23:00',
-//     closed_time: 'ddddddddddd',
-//     closed_day: '휴일 : 수',
-//     category_id: '중국음식',
-//   },
-//   {
-//     id: 4,
-//     name: '차이나',
-//     rate: '4.5',
-//     address: '주소 : 	서울특별시 용산구 한강대로40길 14 1F 104호',
-//     tel: '전화번호 : 	010-2757-3592',
-//     open_time: '영업시간 : 	17:00 - 23:00',
-//     closed_time: 'ddddddddddd',
-//     closed_day: '휴일 : 수',
-//     category_id: '중국음식',
-//   },
-// ];
--- src/Pages/AdminPage/Store.js
+++ src/Pages/AdminPage/Store.js
@@ -1,53 +1,45 @@
-import React, { useEffect, useState } from 'react';
-import { Link, useNavigate, useParams } from 'react-router-dom';
+import React from 'react';
+import { Link } from 'react-router-dom';
 import './Store.scss';
 
-const Store = ({ text }) => {
-  console.log(text);
-
-  // const navigate = useNavigate();
-  // const goToAdminEdit = () => {
-  //   navigate('/admin/edit');
-  // };
-
-  // const params = useParams();
-  // const storeId = params.id;
-
-  // const [detailData, setDetailData] = useState({});
-
-  // useEffect(() => {
-  //   fetch(`http://192.168.215.82:8000/user/admin/${storeId}`)
-  //     .then(response => response.json())
-  //     .then(result => setDetailData(result));
-  // }, [storeId]);
-
-  // console.log(detailData);
+const Store = ({ TEXT }) => {
+  const {
+    image,
+    name,
+    rate,
+    address,
+    tel,
+    open_time,
+    closed_time,
+    closed_day,
+    category_id,
+  } = TEXT;
 
   return (
-    <div className="Store">
+    <div className="store">
       <div className="storeWrap">
-        <img className="storeImg" src={text.image} alt="가게사진" />
+        <img className="storeImg" src={image} alt="가게사진" />
         <div className="storeInfo">
           <div className="mainInfo">
-            <span className="title">{text.name}</span>
-            {text.rate !== 0 ? (
-              <span className="grade">{text.rate}</span>
+            <span className="title">{name}</span>
+            {TEXT.rate !== 0 ? (
+              <span className="grade">{rate}</span>
             ) : (
               <span className='="grade'> 평가가 없습니다.</span>
             )}
           </div>
           <div className="info">
-            <p className="address">주소 : {text.address}</p>
-            <p className="tel">전화번호 : {text.tel}</p>
-            <p className="hour">오픈시간 : {text.open_time}</p>
-            <p className="hour">마감시간 : {text.closed_time}</p>
-            <p className="off ">휴뮤일 : {text.closed_day}</p>
-            <p className="off ">음식종류 : {text.category_id}</p>
+            <p className="address">주소 : {address}</p>
+            <p className="tel">전화번호 : {tel}</p>
+            <p className="hour">오픈시간 : {open_time}</p>
+            <p className="hour">마감시간 : {closed_time}</p>
+            <p className="off ">휴뮤일 : {closed_day}</p>
+            <p className="off ">음식종류 : {category_id}</p>
           </div>
         </div>
       </div>
       <div className="modifyButton">
-        <Link key={text.id} to={`/admin/edit/${text.id}`}>
+        <Link key={TEXT.id} to={`/admin/edit/${TEXT.id}`}>
           <button className="edit">수정하기</button>
         </Link>
       </div>
