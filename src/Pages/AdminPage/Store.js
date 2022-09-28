654465c71181fa9e925e7c298fb683d25b8392dc
SungJongHwa <jonghwasung@gamail.com>
2022-09-28 11:37:34 +0900

    WIP

--- src/Pages/AdminPage/Store.js
+++ src/Pages/AdminPage/Store.js
@@ -22,6 +22,7 @@ const Store = ({ text }) => {
   // }, [storeId]);
 
   // console.log(detailData);
+
   return (
     <div className="Store">
       <div className="storeWrap">
@@ -29,13 +30,17 @@ const Store = ({ text }) => {
         <div className="storeInfo">
           <div className="mainInfo">
             <span className="title">{text.name}</span>
-            <span className="grade">{text.rate}</span>
+            {text.rate !== 0 ? (
+              <span className="grade">{text.rate}</span>
+            ) : (
+              <span className='="grade'> 평가가 없습니다.</span>
+            )}
           </div>
           <div className="info">
             <p className="address">주소 : {text.address}</p>
-            <p className="tel">전화번호 :{text.tel}</p>
+            <p className="tel">전화번호 : {text.tel}</p>
             <p className="hour">오픈시간 : {text.open_time}</p>
-            <p className="hour">마감시간 :{text.closed_time}</p>
+            <p className="hour">마감시간 : {text.closed_time}</p>
             <p className="off ">휴뮤일 : {text.closed_day}</p>
             <p className="off ">음식종류 : {text.category_id}</p>
           </div>
